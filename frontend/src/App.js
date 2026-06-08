44af7ecef98a3c0c4f74e7246cbc3287fdeae47b
lmc000 <mcostatti@gmail.com>
2026-06-08 10:45:21 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Blog from "./pages/Blog";
 import Artigo from "./pages/Artigo";
+import Corretoras from "./pages/Corretoras";
+import Ferramentas from "./pages/Ferramentas";
 import Privacidade from "./pages/Privacidade";
 
 export default function App() {
@@ -11,6 +13,8 @@ export default function App() {
                 <Route path="/" element={<Landing />} />
                 <Route path="/blog" element={<Blog />} />
                 <Route path="/blog/:slug" element={<Artigo />} />
+                <Route path="/corretoras" element={<Corretoras />} />
+                <Route path="/ferramentas" element={<Ferramentas />} />
                 <Route path="/privacidade" element={<Privacidade />} />
             </Routes>
         </BrowserRouter>
