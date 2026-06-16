d1e7d4a8d99e17123f2276020158b11b330ec473
lmc000 <mcostatti@gmail.com>
2026-06-16 09:33:02 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Landing from "./pages/Landing";
 import Blog from "./pages/Blog";
 import Artigo from "./pages/Artigo";
